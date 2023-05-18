0822d8ce9b4bd649015bb0aa3d5d0057a9a17547
Filipe Dias <filipediascontato@gmail.com>
2023-05-18 11:28:32 -0300

    fix: Updates the mobile project

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,9 +4,7 @@ import { Text, View } from 'react-native'
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-zinc-900">
-      <Text className="text-4xl font-bold text-zinc-50">
-        Open up App.js to start working on your app!
-      </Text>
+      <Text className="text-4xl font-bold text-zinc-50">Hello World</Text>
       <StatusBar style="light" translucent />
     </View>
   )
