8cc23036719619a599c8786a0c0676541560daf1
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-05-19 17:41:43 -0300

    feat: Starts to implements the OAuth

---
+++ README.md
@@ -0,0 +1,18 @@
+# NLW Spacetime
+
+## Getting started
+
+Uma vez que o projeto utiliza o GitHub como provedor de autenticação por meio do protocolo OAuth, será necessário configurar um aplicativo OAuth nas configurações da sua conta do GitHub. Dessa forma, você deverá seguir os seguintes passos:
+
+1. Configurar o `GitHub Client Id` nas variáveis de ambiente do projeto `web` como exemplificado no [arquivo .env.example](./web/.env.example) do projeto `web`:
+
+```
+NEXT_PUBLIC_GITHUB_CLIENT_ID="your-github-oauth-app-client-id"
+```
+
+2. Configurar o `GitHub Client Id` e o `GitHub Client Secret` nas variáveis de ambiente do projeto `server` como exemplificado no [arquivo .env.example](./server/.env.example) do projeto `server`:
+
+```
+GITHUB_CLIENT_ID="your-github-oauth-app-client-id"
+GITHUB_CLIENT_SECRET="your-github-oauth-app-client-secret"
+```
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
-import { User } from 'lucide-react'
-import Image from 'next/image'
-import Link from 'next/link'
-import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+import { Copyright } from '@/components/Copyright'
+import { EmptyMemories } from '@/components/EmptyMemories'
+import { Hero } from '@/components/Hero'
+import { SignIn } from '@/components/SignIn'
 
 export default function Home() {
   return (
@@ -9,68 +9,13 @@ export default function Home() {
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
         <div className="absolute inset-y-0 right-2 w-2 bg-stripes" />
-
-        <Link
-          href=""
-          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5" />
-          </div>
-
-          <p className="max-w-[140px] text-sm leading-snug">
-            <span className="underline">Crie sua conta</span> e salve suas
-            memórias!
-          </p>
-        </Link>
-
-        <div className="space-y-5">
-          <Image src={nlwLogo} alt="NLW Spacetime" />
-
-          <div className="max-w-md space-y-4">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-
-            <p className="text-lg leading-relaxed">
-              Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) com o mundo!
-            </p>
-          </div>
-
-          <Link
-            href=""
-            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black transition-colors hover:bg-green-600"
-          >
-            CADASTRAR LEMBRANÇA
-          </Link>
-        </div>
-
-        <div className="text-sm leading-relaxed text-gray-200">
-          Feito com 💜 no NLW da{' '}
-          <a
-            href="https://rocketseat.com.br"
-            target="_blank"
-            rel="noreferrer"
-            className="underline  transition-colors hover:text-gray-100"
-          >
-            Rocketseat
-          </a>
-        </div>
+        <SignIn />
+        <Hero />
+        <Copyright />
       </div>
 
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="w-[360px] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança.{' '}
-            <Link
-              href=""
-              className="underline  transition-colors hover:text-gray-50"
-            >
-              Comece a criar agora!
-            </Link>
-          </p>
-        </div>
+        <EmptyMemories />
       </div>
     </main>
   )
